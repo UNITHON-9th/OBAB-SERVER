665254a00d08f45ea45f4194539e1e35b71becd4
SeungHan <jk05018@naver.com>
2022-09-04 02:17:22 +0900

    refactor: SurveyService 로직 변경

--- src/main/java/dev/unit/obab/survey/service/SurveyService.java
+++ src/main/java/dev/unit/obab/survey/service/SurveyService.java
@@ -2,5 +2,7 @@ package dev.unit.obab.survey.service;
 import dev.unit.obab.survey.domain.SurveyDto;
 
 public interface SurveyService {
+
+    /* 조사 결과 저장 */
     void saveSurveyResult(SurveyDto surveyDto);
 }
