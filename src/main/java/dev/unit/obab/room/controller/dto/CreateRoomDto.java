0b4f880b424356c3e7e600c388e74240bc294b8c
SeungHan <jk05018@naver.com>
2022-09-03 21:32:43 +0900

    Feat: add room validation

--- src/main/java/dev/unit/obab/room/controller/dto/CreateRoomDto.java
+++ src/main/java/dev/unit/obab/room/controller/dto/CreateRoomDto.java
@@ -1,8 +1,14 @@
 package dev.unit.obab.room.controller.dto;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
 import lombok.Getter;
 
 @Getter
 public class CreateRoomDto {
+
+	@Min(1)
+	@Max(100)
 	private int totalCount;
 }
--- src/main/java/dev/unit/obab/room/controller/dto/EnterRoomDto.java
+++ src/main/java/dev/unit/obab/room/controller/dto/EnterRoomDto.java
@@ -1,10 +1,18 @@
 package dev.unit.obab.room.controller.dto;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 import lombok.Getter;
 
 @Getter
 public class EnterRoomDto {
 
+	@Pattern(regexp = "^[0-9]{6}")
+	@NotNull
 	private String inviteCode;
+
+	@NotNull
 	private String deviceId;
 }
